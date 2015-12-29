316e701a834c4ee88ce92f589afb0a364ebd9045
Eran <eranwitkon@gmail.com>
2015-12-29 11:03:58 +0000

    jscs and jshint added close #5

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,12 @@ module.exports = function(grunt) {
     var tasks;
 
     require('load-grunt-initconfig')(grunt);
+    
+    grunt.registerTask('lint', ['jscs', 'jshint']);
 
     
     tasks = [
+        'lint'
         
     ];
     
